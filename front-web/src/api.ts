8f786d74f7e800d5cd6863b4215f67eeef38cea9
Renato <rena.f.oliveira@gmail.com>
2021-01-10 23:17:55 -0300

    Update env

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { OrderPayload } from "./Orders/types";
 
 const API_URL = process.env.REACT_APP_API_URL;
-const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
+const mapboxToken = "pk.eyJ1IjoiYm9kZWdhbWkiLCJhIjoiY2tnOGUycjFoMDB2bzJxcWNydmxqcDJoaSJ9.TaSrHQCH7SjHo-6rPBbNeg";
 
 export function fetchProducts() {
   return axios(`${API_URL}/products`);
